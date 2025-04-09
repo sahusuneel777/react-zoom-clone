fa46df8cd54ab924deb2ad0f6ee1778cb89fb16c
suneel <suneel.sahu@kimshospitals.com>
2025-04-09 22:58:50 +0530

    firebase setup

--- src/utils/firebaseConfig.ts
+++ src/utils/firebaseConfig.ts
@@ -1,26 +1,9 @@
 import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 import { collection, getFirestore } from "firebase/firestore";
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: "react-auth-6788d.firebaseapp.com",
-//   projectId: "react-auth-6788d",
-//   storageBucket: "react-auth-6788d.appspot.com",
-//   messagingSenderId: "131797845021",
-//   appId: "1:131797845021:web:3f7ff4766e2b89ca5d32f4",
-//   measurementId: "G-VWPBR1NSLL",
-// };
 
-// const app = initializeApp(firebaseConfig);
-
-// Import the functions you need from the SDKs you need
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// console.log("first21",process.env.REACT_APP_FIREBASE_API_KEY)
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY, // "AIzaSyDgyqeENAkG3bO4jLML7zOp2RUNIfqT2Ns",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "zoom-clone-9f3ea.firebaseapp.com",
   projectId: "zoom-clone-9f3ea",
   storageBucket: "zoom-clone-9f3ea.firebasestorage.app",
